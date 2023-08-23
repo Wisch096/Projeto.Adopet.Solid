f6759f8add69e9659c1699141ba2b2398cd1c983
Daniel Portugal <dpcosta@gmail.com>
2023-08-23 11:14:02 -0300

    vídeo 1.3

---
+++ Alura.Adopet.Console/Util/LeitorDeArquivoJson.cs
@@ -0,0 +1,19 @@
+﻿using Alura.Adopet.Console.Modelos;
+using System.Text.Json;
+
+namespace Alura.Adopet.Console.Util;
+
+public class LeitorDeArquivoJson : ILeitorDeArquivo
+{
+    private readonly string caminhoArquivo;
+
+    public LeitorDeArquivoJson(string caminhoArquivo) => this.caminhoArquivo = caminhoArquivo;
+
+    public IEnumerable<Pet> RealizaLeitura()
+    {
+        using var stream = new FileStream(caminhoArquivo, FileMode.Open, FileAccess.Read);
+
+        return JsonSerializer.Deserialize<IEnumerable<Pet>>(stream);
+
+    }
+}
