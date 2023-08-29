febaa54a28f3cefd204585a28ec5799778809bc0
Daniel Portugal <dpcosta@gmail.com>
2023-08-29 13:38:39 -0300

    Fazendo os testes passarem

--- tests/Alura.Adopet.Testes/Builder/LeitorDeArquivosMockBuilder.cs
+++ tests/Alura.Adopet.Testes/Builder/LeitorDeArquivosMockBuilder.cs
@@ -1,15 +1,14 @@
 ﻿using Alura.Adopet.Console.Modelos;
-using Alura.Adopet.Console.Servicos.Arquivos;
+using Alura.Adopet.Console.Servicos.Abstracoes;
 using Moq;
 
 namespace Alura.Adopet.Testes.Builder;
 
 internal static class LeitorDeArquivosMockBuilder
 {
-    public static Mock<PetsDoCsv> GetMock(List<Pet> listaDePet)
+    public static Mock<ILeitorDeArquivo<Pet>> GetMock(List<Pet> listaDePet)
     {
-        var leitorDeArquivo = new Mock<PetsDoCsv>(MockBehavior.Default,
-            It.IsAny<string>());
+        var leitorDeArquivo = new Mock<ILeitorDeArquivo<Pet>>(MockBehavior.Default);
 
         leitorDeArquivo.Setup(_ => _.RealizaLeitura()).Returns(listaDePet);
 
