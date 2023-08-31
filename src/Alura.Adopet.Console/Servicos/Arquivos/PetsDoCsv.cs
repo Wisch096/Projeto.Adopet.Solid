8182cd95125140f98bf666ecec075f6052d5d3e5
Daniel Portugal <dpcosta@gmail.com>
2023-08-31 10:12:57 -0300

    Ajuste para que todos testes fiquem verde.

--- src/Alura.Adopet.Console/Servicos/Arquivos/PetsDoCsv.cs
+++ src/Alura.Adopet.Console/Servicos/Arquivos/PetsDoCsv.cs
@@ -4,26 +4,22 @@ namespace Alura.Adopet.Console.Servicos.Arquivos;
 
 public class PetsDoCsv : LeitorArquivoCsv<Pet>
 {
-    
+
     public PetsDoCsv(string caminhoDoArquivoASerLido) : base(caminhoDoArquivoASerLido)
     {
     }
 
     protected override Pet CreateObject(string linha)
     {
-        string[]? propriedades = linha?.Split(';') ?? throw new ArgumentNullException("Texto não pode ser nulo!");
-
-        if (string.IsNullOrEmpty(linha)) throw new ArgumentException("Texto não pode ser vazio");
-
+        string[] propriedades = linha.Split(';');
         bool guidValido = Guid.TryParse(propriedades[0], out Guid petId);
         if (!guidValido) throw new ArgumentException("Identificador do pet inválido!");
 
         bool tipoValido = int.TryParse(propriedades[2], out int tipoPet);
         if (!tipoValido) throw new ArgumentException("Tipo do pet inválido!");
 
-        //int[] enums = Array.ConvertAll(Enum.GetValues<TipoPet>(), value => (int)value);
-        //if (!enums.Contains(tipoPet)) throw new ArgumentException("Tipo do pet inválido!");
+        TipoPet tipo = tipoPet == 1 ? TipoPet.Gato : TipoPet.Cachorro;
 
-        return new Pet(petId, propriedades[1], int.Parse(propriedades[2]) == 1 ? TipoPet.Gato : TipoPet.Cachorro);
+        return new Pet(petId, propriedades[1], tipo);
     }
 }
