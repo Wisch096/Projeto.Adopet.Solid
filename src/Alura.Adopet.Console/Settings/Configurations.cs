4578ebbaed97be4458551cb06c8f818f5322cb0e
Daniel Portugal <dpcosta@gmail.com>
2023-09-01 09:01:12 -0300

    Vídeo 5.3 (secrets)

--- src/Alura.Adopet.Console/Settings/Configurations.cs
+++ src/Alura.Adopet.Console/Settings/Configurations.cs
@@ -9,6 +9,7 @@ public static class Configurations
         var _config = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddUserSecrets("7dd40a68-35b5-40c1-9a17-46dbe7bb4bf6")
             .Build();
 
         return _config
