3b891856539e8b3805c1cef99d3285c441fbf657
Daniel Portugal <dpcosta@gmail.com>
2023-09-04 15:33:49 -0300

    Vídeo 1.2

---
+++ tests/Alura.Adopet.Testes/Servicos/LeitorDeArquivoJsonTest.cs
@@ -0,0 +1,53 @@
+﻿using Alura.Adopet.Console.Servicos.Arquivos;
+
+namespace Alura.Adopet.Testes.Servicos;
+
+public class LeitorDeArquivoJsonTest : IDisposable
+{
+    private readonly string caminhoArquivo;
+
+    public LeitorDeArquivoJsonTest()
+    {
+        //Setup
+        string conteudo = @"
+            [
+              {
+                ""Id"": ""68286fbf-f6f4-4924-adab-0637511813e0"",
+                ""Nome"": ""Mancha"",
+                ""Tipo"": 1
+              },
+              {
+                ""Id"": ""68286fbf-f6f4-4924-adab-0637511672e0"",
+                ""Nome"": ""Alvo"",
+                ""Tipo"": 1
+              },
+              {
+                ""Id"": ""68286fbf-f6f4-1234-adab-0637511672e0"",
+                ""Nome"": ""Pinta"",
+                ""Tipo"": 1
+              }
+            ]
+        ";
+
+        string nomeRandomico = $"{Guid.NewGuid()}.json";
+
+        File.WriteAllText(nomeRandomico, conteudo);
+        caminhoArquivo = Path.GetFullPath(nomeRandomico);
+    }
+
+    [Fact]
+    public void QuandoArquivoExistenteDeveRetornarUmaListaDePets()
+    {
+        //Arrange            
+        //Act
+        var listaDePets = new LeitorDeArquivoJson(caminhoArquivo).RealizaLeitura()!;
+        //Assert
+        Assert.NotNull(listaDePets);
+        Assert.Equal(3, listaDePets.Count());
+    }
+
+    public void Dispose()
+    {
+        File.Delete(caminhoArquivo);
+    }
+}
