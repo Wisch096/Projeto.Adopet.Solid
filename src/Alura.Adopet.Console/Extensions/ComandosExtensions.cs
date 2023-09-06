7e29d3cb63a18b54807a852503bbb7114e9519ab
Daniel Portugal <dpcosta@gmail.com>
2023-09-06 15:30:36 -0300

    Vídeos 4.1 e 4.2

---
+++ src/Alura.Adopet.Console/Extensions/ComandosExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Alura.Adopet.Console.Atributos;
+using Alura.Adopet.Console.Comandos;
+using System.Reflection;
+
+namespace Alura.Adopet.Console.Extensions;
+
+public static class ComandosExtensions
+{
+    public static Type? GetTipoDoComando(this Assembly assembly, string instrucao)
+    {
+        return assembly
+            .GetTypes() // lista de tipos
+                        // filtrar apenas os tipos concretos que são comandos
+            .Where(t => !t.IsInterface && t.IsAssignableTo(typeof(IComando))) // IComando comando = t
+                                                                              // recuperar apenas aquele que atende à instrução "instrucao"
+            .FirstOrDefault(t => t.GetCustomAttributes<DocComandoAttribute>().Any(d => d.Instrucao.Equals(instrucao)));
+    }
+}
---
+++ tests/Alura.Adopet.Testes/Comandos/ComandosExtensionsTest.cs
@@ -0,0 +1,27 @@
+﻿using Alura.Adopet.Console.Comandos;
+using Alura.Adopet.Console.Extensions;
+using System.Reflection;
+
+namespace Alura.Adopet.Testes.Comandos;
+
+public class ComandosExtensionsTest
+{
+    [Theory]
+    [InlineData("import", "Import")]
+    [InlineData("import-clientes", "ImportClientes")]
+    [InlineData("show", "Show")]
+    [InlineData("list", "List")]
+    [InlineData("help", "Help")]
+    public void QuandoInstrucaoValidaDeveRetornarUmTipoComando(string instrucao, string tipoEsperado)
+    {
+        // arrange
+        Assembly assembly = Assembly.GetAssembly(typeof(Import))!;
+
+        // act
+        Type? tipoRetornado = assembly.GetTipoDoComando(instrucao);
+
+        // assert
+        Assert.NotNull(tipoRetornado);
+        Assert.Equal(tipoRetornado.Name, tipoEsperado);
+    }
+}
