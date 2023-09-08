9cc4caae335e7c156359cd82659c5b334b91fa29
Daniel Portugal <dpcosta@gmail.com>
2023-09-08 10:21:02 -0300

    Vídeo 5.1

--- src/Alura.Adopet.Console/Comandos/ImportFactory.cs
+++ src/Alura.Adopet.Console/Comandos/ImportFactory.cs
@@ -1,5 +1,8 @@
-﻿using Alura.Adopet.Console.Servicos.Arquivos;
+﻿using Alura.Adopet.Console.Servicos.Abstracoes;
+using Alura.Adopet.Console.Servicos.Arquivos;
 using Alura.Adopet.Console.Servicos.Http;
+using Alura.Adopet.Console.Servicos.Mail;
+using System.Net.Mail;
 
 namespace Alura.Adopet.Console.Comandos;
 
@@ -9,8 +12,20 @@ public class ImportFactory : IComandoFactory
     {
         return tipoComando?.IsAssignableTo(typeof(Import)) ?? false;
     }
+
+    private IMailService CriarMailService()
+    {
+        SmtpClient smtpClient = new()
+        {
+
+        };
+        return new SmtpClientMailService(smtpClient);
+    }
+
+
     public IComando? CriarComando(string[] argumentos)
     {
+        IMailService mailService = CriarMailService();
         var httpClientPet = new HttpClientPet(new AdopetAPIClientFactory().CreateClient("adopet"));
         var leitorDeArquivos = LeitorDeArquivoFactory.CreateLeitorPetFrom(argumentos[1]);
         if (leitorDeArquivos is null) return null;
