e3576e9d8525ba049f84ea74a00b2abf8f20f47e
bessax <andrebessax@gmail.com>
2023-09-12 11:13:16 -0300

    #Vídeo 1.2:

---
+++ src/Alura.Adopet.Console/Servicos/Arquivos/LeitorDeArquivosJson.cs
@@ -0,0 +1,18 @@
+﻿using Alura.Adopet.Console.Modelos;
+using System.Text.Json;
+
+namespace Alura.Adopet.Console.Servicos.Arquivos;
+public class LeitorDeArquivosJson
+{
+    private string caminhoArquivo;
+    public LeitorDeArquivosJson(string caminhoArquivo)
+    {
+        this.caminhoArquivo = caminhoArquivo;
+    }
+
+    public IEnumerable<Pet> RealizaLeitura()
+    {
+        using var stream = new FileStream(caminhoArquivo, FileMode.Open, FileAccess.Read);
+        return JsonSerializer.Deserialize<IEnumerable<Pet>>(stream);
+    }
+}
